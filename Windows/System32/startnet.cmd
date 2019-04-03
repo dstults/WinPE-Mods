db7dbabe6834c739a647caa297286e2cc4c757ba
Darren <drankof@gmail.com>
2019-04-03 10:53:07 -0700

    Update startnet.cmd
    => error fixed

--- Windows/System32/startnet.cmd
+++ Windows/System32/startnet.cmd
@@ -7,15 +7,15 @@ echo ipconfig /all
 echo netstat /a /n
 echo.
 echo Custom Batch Shortcuts:
-echo netsh => netset / netcustom
-echo wpeutil => shutdown / restart
+echo netset / netcustom (netsh)
+echo shutdown / restart (wpeutil)
 echo.
 echo Custom Applications:
-echo explore => GUI file explorer
-echo notepad++ => better notepad
-echo 7zip => 7-Zip file manager (archive compression and extraction)
-echo putty => telnet/ssh client
+echo explore - GUI file explorer
+echo notepad++ - better notepad
+echo 7zip - 7-Zip file manager (archive compression and extraction)
+echo putty - telnet/ssh client
 echo.
-echo Most important:
-echo All your base are belong to us.
+echo Most important:"
+echo All your base are belong to us."
 echo.
